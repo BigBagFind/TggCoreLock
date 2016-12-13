f13dfb2311c547a656434aab2a790b0ac38bfb85
wuyt <594228924@qq.com>
2016-12-13 23:41:26 +0800

    Initial Commit

---
+++ TggCoreLock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TggCoreLock
+//
+//  Created by 吴玉铁 on 2016/12/13.
+//  Copyright © 2016年 吴玉铁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TggCoreLock/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TggCoreLock
+//
+//  Created by 吴玉铁 on 2016/12/13.
+//  Copyright © 2016年 吴玉铁. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
